56db469e0545e76b02ed7b913ae2472b500e6208
minhtn2310@gmail.com <minhpro111>
2021-11-24 09:36:15 +0700

    create project

---
+++ src/main/java/com/springboot/vote/SimpleVoteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/vote/SimpleVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
